2fd8af91a63125e54b94a0804c7c356c1d76a3ac
vignesh-27 <33652569+vignesh-27@users.noreply.github.com>
2017-11-27 11:54:13 +0530

    Update task.js

--- controllers/task.js
+++ controllers/task.js
@@ -1,18 +1,10 @@
 'use strict' //Applying strict mode to the file format
-
-var data = [{name:'Index', link:'/'},{name:'Career', link:'/career'},
-           {name:'Contact Us', link:'/contact'}];
-
-var font = {margin:'margin-top:120px;', size:'font-size:40px;',family:'font-family:olivia;', color:'color:orange;'};
-
-var img  = {background:'/assets/images/bg6.jpeg', logo:'/assets/images/adapt-logo.png',  margin_l:'margin-left: 400px;',
-           margin_t:'margin-top: 60px;', height:'height : 350px;', width:'width : 500px;', opacity:'opacity : 0.4;'};
-
+var obj = require('./value.json'); //including json data objects
 
 module.exports = (app)=>{
 
 app.get('/',  (req,res)=>{
-    res.render('index', {data: data, font:font, image:img});
+    res.render('index', {data: obj.data, font:obj.font, image:obj.img});
 });
 
 app.get('/career', (req, res)=>{
